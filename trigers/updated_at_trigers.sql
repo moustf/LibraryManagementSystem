321446ad7ffbe36fa3f10a02bb87cbc995cb1763
Mustafa Salem <mustafaissasalem@gmail.com>
2023-08-28 21:58:20 +0300

    refactor: add a condition to check if the triggers exist before creating them
    Refactor the tables where clause
    Formatted the code
    
    Close: #4

--- trigers/updated_at_trigers.sql
+++ trigers/updated_at_trigers.sql
@@ -1,41 +1,62 @@
+USE library_management_system;
+
 -- Creating a trigger to update the updated_At field in the book table automatically.
-CREATE TRIGGER update_book_updated_at
-ON book
-AFTER UPDATE
-AS
-BEGIN
-    UPDATE book
-    SET updated_at = GETDATE()
-    FROM book AS b
-    INNER JOIN inserted AS i ON b.book_id = i.book_id
-END;
+IF NOT EXISTS
+    (SELECT 1
+     FROM sys.triggers
+     WHERE name = 'update_book_updated_at'
+       AND parent_id = OBJECT_ID('dbo.book'))
+    BEGIN
+        EXEC ('CREATE TRIGGER dbo.update_book_updated_at
+            ON dbo.book
+            AFTER UPDATE
+            AS
+        BEGIN
+            UPDATE book
+            SET updated_at = GETDATE()
+            FROM book AS b
+                     INNER JOIN inserted AS i ON b.book_id = i.book_id
+        END;');
+    END;
 
 GO
 
 -- Creating a trigger to update the updated_At field in the borrower table automatically.
-CREATE TRIGGER update_borrower_updated_at
-ON borrower
-AFTER UPDATE
-AS
-BEGIN
-    UPDATE borrower
-    SET updated_at = GETDATE()
-    FROM borrower AS b
-    INNER JOIN inserted AS i On b.borrower_id = i.borrower_id
-END;
+IF NOT EXISTS(SELECT 1
+              FROM sys.triggers
+              WHERE name = 'update_borrower_updated_at'
+                AND parent_id = OBJECT_ID('dbo.borrower'))
+    BEGIN
+        EXEC ('CREATE TRIGGER update_borrower_updated_at
+            ON borrower
+            AFTER UPDATE
+            AS
+        BEGIN
+            UPDATE borrower
+            SET updated_at = GETDATE()
+            FROM borrower AS b
+                     INNER JOIN inserted AS i On b.borrower_id = i.borrower_id
+        END;');
+    END;
 
 GO
 
 -- Creating a trigger to update the updated_At field in the borrower table automatically.
-CREATE TRIGGER update_loan_updated_at
-ON loan
-AFTER UPDATE
-AS
-BEGIN
-    UPDATE loan
-    SET updated_at = GETDATE()
-    FROM loan AS l
-    INNER JOIN inserted AS i On l.loan_id = i.loan_id
-END;
+IF NOT EXISTS(SELECT *
+              FROM sys.triggers
+              WHERE name = 'update_loan_updated_at'
+                AND parent_id = OBJECT_ID('dbo.loan'))
+    BEGIN
+        EXEC ('CREATE TRIGGER update_loan_updated_at
+            ON loan
+            AFTER UPDATE
+            AS
+        BEGIN
+            UPDATE loan
+            SET updated_at = GETDATE()
+            FROM loan AS l
+                     INNER JOIN inserted AS i On l.loan_id = i.loan_id
+        END;');
+    END;
 
 GO
