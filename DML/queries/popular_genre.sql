ed9def5ca9ccdf5a99513785d8f3b4656f95ac30
Mustafa Salem <mustafaissasalem@gmail.com>
2023-08-29 17:36:27 +0300

    refactor: add a condition for checking if the procedure exists or not to avoid rasing errors
    Close: #8

--- DML/queries/popular_genre.sql
+++ DML/queries/popular_genre.sql
@@ -1,17 +1,22 @@
-CREATE PROCEDURE sp_PopularGenreForAGivenMonth(
-    @month INT
-)
-AS
-BEGIN
-    SELECT TOP 1 COUNT(genre) AS Count,
-                 genre        AS Genre
-    FROM book AS b
-             INNER JOIN loan AS l
-                        ON b.book_id = l.book_id
-    WHERE DATEPART(MONTH, l.date_borrowed) = @month
+IF NOT EXISTS(SELECT 1
+              FROM sys.procedures
+              WHERE name = 'sp_PopularGenreForAGivenMonth')
+    BEGIN
+        EXEC ('CREATE PROCEDURE sp_PopularGenreForAGivenMonth(
+                    @month INT
+                )
+                AS
+                BEGIN
+                    SELECT TOP 1 COUNT(genre) AS Count,
+                                 genre        AS Genre
+                    FROM book AS b
+                             INNER JOIN loan AS l
+                                        ON b.book_id = l.book_id
+                    WHERE DATEPART(MONTH, l.date_borrowed) = @month
 
-    GROUP BY genre
-    ORDER BY Count DESC;
-END ;
+                    GROUP BY genre
+                    ORDER BY Count DESC;
+                END ;');
+    END;
 
-    EXEC sp_PopularGenreForAGivenMonth 5;
+EXEC sp_PopularGenreForAGivenMonth 5;
